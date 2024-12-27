e88d4adfc510f9f8e7e725071d72a7416c557683
dak2 <dawt2h@gmail.com>
2024-12-27 07:09:28 +0000

    3.2.5: output filename and open it

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,11 +1,27 @@
-use std::error::Error;
 use clap::{App, Arg};
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 pub fn run(config: Config) -> MyResult<()> {
-    dbg!(config);
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(_) => eprintln!("Opened {}", filename),
+        }
+    }
     Ok(())
+}
+
+// Store the heap memory pointer to the stack memory, because the dyn Error does not know size of retrun value
+// https://doc.rust-jp.rs/book-ja/ch15-01-box.html#%E3%83%92%E3%83%BC%E3%83%97%E3%81%AE%E3%83%87%E3%83%BC%E3%82%BF%E3%82%92%E6%8C%87%E3%81%99boxt%E3%82%92%E4%BD%BF%E7%94%A8%E3%81%99%E3%82%8B
+pub fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
 
 pub fn get_args() -> MyResult<Config> {

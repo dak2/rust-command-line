7ac0108a9254b7965ff474fe41ea727d850a8f28
dak2 <dawt2h@gmail.com>
2025-02-01 08:14:32 +0000

    6.2.1: pass arguments

---
+++ uniqr/src/lib.rs
@@ -0,0 +1,52 @@
+use clap::{App, Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+  in_file: String,
+  out_file: Option<String>,
+  count: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+  let matches = App::new("uniqr")
+    .version("0.1.0")
+    .author("dak2")
+    .about("Rust uniq")
+    .arg(
+        Arg::with_name("count")
+            .short("c")
+            .long("count")
+            .help("Show counts")
+            .takes_value(false)
+    )
+    .arg(
+        Arg::with_name("in_file")
+            .value_name("IN_FILE")
+            .help("Input file")
+            .default_value("-")
+    )
+    .arg(
+        Arg::with_name("out_file")
+            .value_name("OUT_FILE")
+            .help("Output file")
+    )
+    .get_matches();
+
+    let count = matches.is_present("count");
+    let in_file = matches.value_of("in_file").unwrap().to_string();
+    let out_file = matches.value_of("out_file").map(String::from);
+
+    Ok(Config {
+        in_file,
+        out_file,
+        count
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+  println!("{:?}", config);
+  Ok(())
+}
